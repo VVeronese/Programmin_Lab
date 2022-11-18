9c059e99f00a40e603aed4dbcd857b9b18e1449e
Virginia Veronese <115220886+VVeronese@users.noreply.github.com>
2022-11-18 11:55:55 +0000

    lezione 3 correzione

--- 17-11/lezione3.py
+++ 17-11/lezione3.py
@@ -1,8 +1,11 @@
 values = [] 
-my_file = open ('shanpoo_sales.csv', 'r')
+my_file = open ('shampoo_sales.csv', 'r')
 for line in my_file:
     elements = line.split (',')
     if elements[0] != 'Date':
-        date = elemnts[0]
+        date = elements[0]
         value = elements[1]
+        
         values.append(value)
+
+print(values)
