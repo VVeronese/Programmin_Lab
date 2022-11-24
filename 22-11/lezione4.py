8009f2fd79689b1ea315e2984e378a4f0476b3ed
Virginia Veronese <115220886+VVeronese@users.noreply.github.com>
2022-11-24 08:38:22 +0000

    First try

--- 22-11/lezione4.py
+++ 22-11/lezione4.py
@@ -1,6 +1,15 @@
 class CSVFile():
-    csvfile 
+    
     def __init__(self, name):
-        self.name = name
-    def get_data():
+        self.name = name #attributo che contiene il nome 
+    def get_data(my_file):
+        file = open (my_file, 'r') #apro my_file nella funzione
+        values = []
+        for line in file:
+            lines = line.split ('\n')  #split delle linee nel file
+            lines.append(list(values)) #ritorno ogni linea in una lista
+        return values; #ritorno le liste
+        
+   
+   
     
