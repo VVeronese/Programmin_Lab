bc3a6ba0101a1572fb2d095f89095dd321b55222
Virginia Veronese <115220886+VVeronese@users.noreply.github.com>
2022-11-24 09:57:43 +0000

    added variable (item) to store lines, append item to lists

--- 22-11/lezione4.py
+++ 22-11/lezione4.py
@@ -1,15 +1,21 @@
 class CSVFile():
     
     def __init__(self, name):
-        self.name = name #attributo che contiene il nome 
-    def get_data(my_file):
-        file = open (my_file, 'r') #apro my_file nella funzione
-        values = []
-        for line in file:
-            lines = line.split ()  #split delle linee nel file
-            lines.append(list((values))) #ritorno ogni linea in una lista
-        return values; #ritorno le liste
         
-   
+        self.name = name #attributo che contiene il nome 
+        
+    def get_data(self, my_file):
+       
+        file = open (my_file, 'r') #apro my_file nella funzione
+        lists = []
+        for line in file:
+            lines = line.split()  #split delle linee nel file
+            item = lines
+            lists.append(item) #ritorno ogni linea in una lista
+        return lists; #ritorno le liste
+
+
+
+#print(CSVFile().get_data('shampoo_sales.csv'))
    
     
